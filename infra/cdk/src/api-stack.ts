201653bf1ef4a49dd101beb9012cf3f63674191b
Fredrik Mörk <fredrik@alcedo.com>
2025-09-15 10:27:55 +0200

    chore: change deployment resource types for API gateway/lambda

--- infra/cdk/src/api-stack.ts
+++ infra/cdk/src/api-stack.ts
@@ -1,19 +1,14 @@
 import type { StackProps } from 'aws-cdk-lib';
 import { CfnOutput, Duration, Stack } from 'aws-cdk-lib';
-import {
-  ApiMapping,
-  CorsHttpMethod,
-  DomainName,
-  HttpApi,
-  HttpMethod,
-} from 'aws-cdk-lib/aws-apigatewayv2';
-import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { Cors, EndpointType, LambdaRestApi, SecurityPolicy } from 'aws-cdk-lib/aws-apigateway';
 import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
 import type { Table } from 'aws-cdk-lib/aws-dynamodb';
 import type { EventBus } from 'aws-cdk-lib/aws-events';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, OutputFormat } from 'aws-cdk-lib/aws-lambda-nodejs';
+import type { IHostedZone } from 'aws-cdk-lib/aws-route53';
 import { ARecord, AaaaRecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
-import { ApiGatewayv2DomainProperties } from 'aws-cdk-lib/aws-route53-targets';
+import { ApiGatewayDomain } from 'aws-cdk-lib/aws-route53-targets';
 import type { Construct } from 'constructs';
 import path from 'path';
 import type { DomainConfig } from './domain';
@@ -30,14 +25,25 @@ export class ApiStack extends Stack {
 
     const __dirnameLocal = __dirname;
 
-    // Use prebuilt artifact from Option A bundling
-    const assetsDir = path.resolve(__dirnameLocal, '../assets/lambdas/api-http-handler');
-    const handler = new lambda.Function(this, 'ApiHandler', {
-      code: lambda.Code.fromAsset(assetsDir),
-      handler: 'index.main',
+    // Bundle Lambda directly with NodejsFunction (esbuild)
+    const apiEntry = path.resolve(
+      __dirnameLocal,
+      '../../../packages/api/src/functions/http-handler/handler.ts',
+    );
+    const tsconfigPath = path.resolve(__dirnameLocal, '../../../tsconfig.base.json');
+    const handler = new NodejsFunction(this, 'ApiHandler', {
+      entry: apiEntry,
+      handler: 'main',
       runtime: lambda.Runtime.NODEJS_20_X,
       memorySize: 512,
       timeout: Duration.seconds(15),
+      bundling: {
+        sourceMap: true,
+        minify: true,
+        format: OutputFormat.ESM,
+        target: 'node20',
+        tsconfig: tsconfigPath,
+      },
       environment: {
         NODE_OPTIONS: '--enable-source-maps',
         TABLE_NAME: props.table.tableName,
@@ -49,73 +55,55 @@ export class ApiStack extends Stack {
     props.table.grantReadWriteData(handler);
     props.eventBus.grantPutEventsTo(handler);
 
-    const integration = new HttpLambdaIntegration('ApiIntegration', handler);
-
-    const api = new HttpApi(this, 'HttpApi', {
-      createDefaultStage: true,
-      corsPreflight: {
-        allowCredentials: false,
-        allowHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-        allowMethods: [
-          CorsHttpMethod.GET,
-          CorsHttpMethod.POST,
-          CorsHttpMethod.PUT,
-          CorsHttpMethod.DELETE,
-          CorsHttpMethod.OPTIONS,
-        ],
-        allowOrigins: ['*'],
-        maxAge: Duration.days(10),
-      },
-    });
-
-    // Proxy all routes to Lambda
-    api.addRoutes({
-      path: '/{proxy+}',
-      methods: [HttpMethod.ANY],
-      integration,
-    });
-
-    // Custom domain for HTTP API + Route53 alias records
+    // Custom domain setup (certificate + hosted zone) if configured
+    let zone: IHostedZone | undefined;
+    let apiCert: Certificate | undefined;
     if (props.domainConfig) {
-      const zone = HostedZone.fromLookup(this, 'ApiHostedZone', {
+      zone = HostedZone.fromLookup(this, 'ApiHostedZone', {
         domainName: props.domainConfig.baseDomain,
       });
-
-      // Regional ACM certificate is required for API Gateway custom domain (must be in same region)
-      const apiCert = new Certificate(this, 'ApiAcmCertificate', {
+      apiCert = new Certificate(this, 'ApiAcmCertificate', {
         domainName: props.domainConfig.apiHost,
         validation: CertificateValidation.fromDns(zone),
       });
+    }
 
-      const customDomain = new DomainName(this, 'ApiCustomDomain', {
-        domainName: props.domainConfig.apiHost,
-        certificate: apiCert,
-      });
+    const api = new LambdaRestApi(this, 'RestApi', {
+      handler,
+      proxy: true,
+      deployOptions: { stageName: 'prod' },
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+        allowHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+        disableCache: false,
+        maxAge: Duration.days(10),
+      },
+      ...(props.domainConfig && apiCert
+        ? {
+            domainName: {
+              domainName: props.domainConfig.apiHost,
+              certificate: apiCert,
+              endpointType: EndpointType.REGIONAL,
+              securityPolicy: SecurityPolicy.TLS_1_2,
+              basePath: '',
+            },
+          }
+        : {}),
+    });
 
-      const defaultStage = api.defaultStage;
-      if (!defaultStage) {
-        throw new Error('HTTP API default stage was not created');
-      }
-      new ApiMapping(this, 'ApiDefaultMapping', {
-        api,
-        domainName: customDomain,
-        stage: defaultStage,
-      });
-
+    // Route53 alias for custom domain
+    if (props.domainConfig && api.domainName && zone) {
+      const domainResource = api.domainName;
       const recordName = props.domainConfig.apiHost.replace(
         `.${props.domainConfig.baseDomain}`,
         '',
       );
-      const target = RecordTarget.fromAlias(
-        new ApiGatewayv2DomainProperties(
-          customDomain.regionalDomainName,
-          customDomain.regionalHostedZoneId,
-        ),
-      );
+      const target = RecordTarget.fromAlias(new ApiGatewayDomain(domainResource));
       new ARecord(this, 'ApiAliasA', { zone, recordName, target });
       new AaaaRecord(this, 'ApiAliasAAAA', { zone, recordName, target });
     }
 
-    new CfnOutput(this, 'ApiEndpoint', { value: api.apiEndpoint });
+    new CfnOutput(this, 'ApiEndpoint', { value: api.url });
   }
 }
