ea29a4da9f72af054f4fe64d8f303984dc0d745f
Fredrik Mörk <fredrik@alcedo.com>
2025-09-15 12:02:02 +0200

    chore: delete bff package

--- packages/bff/src/functions/http-handler/handler.ts
+++
@@ -1,9 +0,0 @@
-import ServerlessHttp from 'serverless-http';
-import { server, logWriter } from '../../init/BffInit';
-
-logWriter.info('Loading BFF handler');
-
-const app = server.getExpressApp();
-
-export const main = ServerlessHttp(app);
-
