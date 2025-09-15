2a7b8d1ffbd0a22257b3abed4f3a340f8860e28a
Fredrik Mörk <fredrik@alcedo.com>
2025-09-15 12:25:40 +0200

    chore: remove unused code

--- packages/api/src/bff/functions/http-handler/handler.ts
+++
@@ -1,8 +0,0 @@
-import ServerlessHttp from 'serverless-http';
-import { logWriter, server } from '../../../init/ApiInit';
-
-logWriter.info('Loading unified API+BFF handler');
-
-const app = server.getExpressApp();
-
-export const main = ServerlessHttp(app);
